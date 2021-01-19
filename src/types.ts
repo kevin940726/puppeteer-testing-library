b8f98f8bc16dbe9a13a91f021fc78b6431491b2d
Kai Hao <kevin830726@gmail.com>
2021-01-19 14:22:07 +0800

    Rewrite Query types to allow RegExp in some fields

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,29 @@
-import { ElementHandle, Page, AXNode } from 'puppeteer';
-
-interface OptionalQuery
-  extends Omit<Omit<Partial<AXNode>, 'children'>, 'name'> {
+interface OptionalQuery {
+  role?: string;
   name?: string | RegExp;
+  value?: string | number | RegExp;
+  description?: string | RegExp;
+  keyshortcuts?: string;
+  roledescription?: string | RegExp;
+  valuetext?: string | RegExp;
+  disabled?: boolean;
+  expanded?: boolean;
+  focused?: boolean;
+  modal?: boolean;
+  multiline?: boolean;
+  multiselectable?: boolean;
+  readonly?: boolean;
+  required?: boolean;
+  selected?: boolean;
+  checked?: boolean | 'mixed';
+  pressed?: boolean | 'mixed';
+  level?: number;
+  valuemin?: number;
+  valuemax?: number;
+  autocomplete?: string;
+  haspopup?: string;
+  invalid?: string;
+  orientation?: string;
   selector?: string;
   text?: string | RegExp;
 }
